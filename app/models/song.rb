22040d4e3f27645cba1c8558c86a53f49123336a
WILLEM SINNIGE <W.SINNIGE@GMAIL.COM>
2017-09-13 18:23:12 +0200

    Song spec finished

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,6 @@
 class Song < ApplicationRecord
   belongs_to :artist
+  validates :title,           presence: true
+  validates :album,           presence: true
+  validates :video_url,       presence: true
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
-factory :song do
-  title              ""
-  album              ""
-  video_url          ""
+  RSpec.describe Song, type: :model do
+    describe "validations" do
+      it "is invalid without a title" do
+        song = Song.new(title: "")
+        song.valid?
+          expect(song.errors).to have_key(:title)
+        end
+      it "is invalid without a title" do
+          song = Song.new(album: "")
+            song.valid?
+              expect(song.errors).to have_key(:album)
+            end
+      it "is invalid without a title" do
+        song = Song.new(video_url: "")
+          song.valid?
+            expect(song.errors).to have_key(:video_url)
+        end
+end
 end
