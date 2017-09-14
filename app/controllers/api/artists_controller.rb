9c904871d291caea1ff0c5f5e14b6d6d726f939d
WILLEM SINNIGE <W.SINNIGE@GMAIL.COM>
2017-09-14 21:05:36 +0200

    api songcontroller

---
+++ app/controllers/api/artists_controller.rb
@@ -0,0 +1,22 @@
+class Api::ArtistsController < ApplicationController
+  def index
+    render status: 200, json: {
+      artists: Artist.all
+    }.to_json
+  end
+  def show
+    artist = Artist.find(params[:id])
+
+    render status: 200, json: {
+      artist: artist
+    }.to_json
+  end
+  def destroy
+    artist = Artist.find(params[:id])
+    artist.destroy
+
+    render status: 200, json: {
+      message: "Artist successfully deleted"
+    }.to_json
+  end
+end
--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,7 +1,42 @@
 class Api::SongsController < ApplicationController
+
   def index
     render status: 200, json: {
       songs: Song.all
     }.to_json
+  end
+
+  def show
+    song = Song.find(params[:id])
+
+    render status: 200, json: {
+      song: song
+    }.to_json
+  end
+  def create
+    @Song = Song.new(Song_params)
+
+    respond_to do |format|
+      if @Song.save
+        format.html { redirect_to artist_songs_path, notice: 'Song was successfully created.' }
+        format.json { render :show, status: :created, location: @Song }
+      else
+        format.html { redirect_to artist_songs_path }
+        format.json { render json: @Song.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  def destroy
+    song = Song.find(params[:id])
+    song.destroy
+
+    render status: 200, json: {
+      message: "Song successfully deleted"
+    }.to_json
+  end
+
+  private
+  def song_params
+    params.require(:song).permit(:title, :album, :video_url)
   end
 end
