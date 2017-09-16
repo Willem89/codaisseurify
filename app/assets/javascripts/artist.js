90fda5692812543e0c1707169873eaabbb8353fc
WILLEM SINNIGE <W.SINNIGE@GMAIL.COM>
2017-09-16 20:23:39 +0200

    A new start

--- app/assets/javascripts/artist.js
+++ app/assets/javascripts/artist.js
@@ -1,47 +1,4 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-});
+$(document).ready(function(){
+  console.log('Ready.....')
 
-function submitSong(event) {
-  // stop the form from doing the default action, submitting...
-  event.preventDefault();
-
-  var title = $("#new-song").val();
-
-  createSong(title);
-
-  $("#new-song").val(null);
-  updateCounters();
-}
-
-
-
-
-function deleteSong(SongId) {
-  event.preventDefault();
-  $.when($(".delete").remove())
-    .then();
-}
-
-
-function createSong(title) {
-  var newSong = {title: title, album:album, video_url:video_url}
-
-
-  .done(function(data) {
-    $('tr[data-id="'+SongId+'"]').remove();
-    updateCounters();
-  });
-}
-
-function DeleteSongs(event) {
-  event.preventDefault();
-
-  $.each($(".song"), function(index, tableRow) {
-    songId = $(tableRow).data('id');
-    deleteSongs(songId);
-  });
-}
-$(document).ready(function() {
-  $("input[type=checkbox]").bind('change', toggleDone);
 });
