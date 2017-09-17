a4121cd7aeb127b71c1a781d0b86f2100c904936
WILLEM SINNIGE <W.SINNIGE@GMAIL.COM>
2017-09-17 13:57:40 +0200

    added Ajax

--- app/assets/javascripts/artist.js
+++ app/assets/javascripts/artist.js
@@ -1,4 +1,42 @@
 $(document).ready(function(){
-  console.log('Ready.....')
+  console.log('deady.....')
 
 });
+
+$.(document)createSong(songId)
+
+
+$.ajax({
+    type: "PUT",
+    url: "/artist/" + artistId + "/song/" + songId + ".json",
+    data: JSON.stringify({
+      todo: { completed: isCompleted }
+    }),
+    contentType: "application/json",
+    dataType: "json"
+  })
+  .done(function(data) {
+    console.log(data);
+
+    tableRow.toggleClass("success", data.completed);
+
+    updateCounters();
+  });
+}
+
+
+
+
+function deleteSong(songId) {
+  
+  $.ajax({
+    type: "DELETE",
+    url: "/artist/" + artistId + "/song/" + songId + ".json",
+    contentType: "application/json",
+    dataType: "json"
+  })
+  .done(function(data) {
+    $('tr[data-id="'+songId+'"]').remove();
+    updateCounters();
+  });
+}
--- spec/features/manage_tasks_spec.rb
+++ spec/features/manage_tasks_spec.rb
@@ -1,16 +1,9 @@
 require 'rails_helper'
 
 feature 'Manage tasks', js: true do
-scenario 'delete a song' do
-artist5 = Artist.create!(first_name: "Willem", last_name: "Sinnige", genre: "Blues", image: "https://cdn.pixabay.com/photo/2015/07/06/15/12/w-833386_960_720.png")
+  scenario 'remove song' do
+      visit artist_path(artist.id)
+      click_on 'Delete song'
 
-song88 = Song.create!(title:"I Do", album:"Guillermo", video_url:"www.youtube.com", artist:artist5)
-
-visit artist_url(artist.id);
-
-page.execute_script("$('#delete').click()");
-
-expect(page).not_to have_content('Damn');
-
-end
+    end
 end
