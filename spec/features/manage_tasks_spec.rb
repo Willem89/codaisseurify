8888242d0dd85ef89ca2d42b5656b8b92f725b5d
WILLEM SINNIGE <W.SINNIGE@GMAIL.COM>
2017-09-17 19:48:52 +0200

    Test suite isnt working yet

--- spec/features/manage_tasks_spec.rb
+++ spec/features/manage_tasks_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 feature 'Manage tasks', js: true do
-  scenario 'remove song' do
-      visit artist_path(artist.id)
-      click_on 'Delete song'
+artist5 = Artist.create!(first_name: "Willem", last_name: "Sinnige", genre: "Blues", image: "https://cdn.pixabay.com/photo/2015/07/06/15/12/w-833386_960_720.png")
 
-    end
+song88 = Song.create!(title:"I Do", album:"Guillermo", video_url:"www.youtube.com", artist:artist5)
+
+scenario 'delete song' do
+   visit artist_path(5)
+   click_link('delete song')
+   expect(page).not_to have_content('I do')
 end
+scenario 'add a song' do
+   visit artist_path(5)
+   fill_in 'form_title', with: 'test song'
+   fill_in 'form_album', with: 'test album'
+   fill_in 'form_video_url', with: 'test.com'
+   click_submit('Add')
+   expect(page).to have_content('test song')
+ end
+ end
